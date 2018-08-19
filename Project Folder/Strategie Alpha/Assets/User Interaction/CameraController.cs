9e99f7275cde734fea1fc32d585758d3121d4886
MeisterGitarrenheld <tom-haeschke@hotmail.de>
2018-08-19 19:18:53 +0200

    Camera Movement

---
+++ Project Folder/Strategie Alpha/Assets/User Interaction/CameraController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        float moveX = Input.GetAxis("Horizontal");
+        float moveY = Input.GetAxis("Vertical");
+
+        transform.position += transform.right * moveX;
+        transform.position += new Vector3(transform.up.x * moveY, 0, transform.up.z * moveY);
+
+	}
+}
