5a9ecc3725f6563c4781dbec32dd00120938b696
DESKTOP-UKF6Q2T\Marco <marco.piechotta@gmail.com>
2018-08-19 16:56:51 +0200

    UnitScene

---
+++ Project Folder/Strategie Alpha/Assets/Scripts/Unit.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Unit : MonoBehaviour {
+	public enum UnitType {
+		BOW_UNIT,
+		AXE_UNIT,
+		COLLECTOR_UNIT
+	}
+	public float MovementSpeed;
+	public float AttackSpeed;
+	public int Health;
+	public UnitType Type;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
