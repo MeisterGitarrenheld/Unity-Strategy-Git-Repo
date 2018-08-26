9215b22d01f14e9ea9b2a0f73027b4b3efb5ed3e
MeisterGitarrenheld <tom-haeschke@hotmail.de>
2018-08-26 17:54:38 +0200

    Bow Attack

--- Project Folder/Strategie Alpha/Assets/Scripts/BowUnit.cs
+++ Project Folder/Strategie Alpha/Assets/Scripts/BowUnit.cs
@@ -4,23 +4,62 @@ using UnityEngine;
 
 public class BowUnit : Unit, HitInterface
 {
+    private HitInterface HitTarget;
+    private float attackTimer;
 
-	// Update is called once per frame
-	override
-	public void updateUnit() {
-        if (target != null)
+    // Update is called once per frame
+    override
+    public void updateUnit()
+    {
+        if (target != null && !dead)
         {
-            agent.SetDestination(target.getTargetPosition());
+            if (target.WType == WType.Attack && target.attackTarget != null)
+            {
+                if (HitTarget == null)
+                    HitTarget = target.attackTarget.GetComponent<HitInterface>();
+                if (Vector3.Distance(transform.position, target.getTargetPosition()) > AttackRange)
+                {
+                    agent.isStopped = false;
+                    agent.SetDestination(target.getTargetPosition());
+                }
+                else
+                {
+                    agent.isStopped = true;
+                    attack();
+                }
+            }
+            else if (target.WType == WType.Walk && Vector3.Distance(transform.position, target.getTargetPosition()) > 1)
+            {
+                agent.isStopped = false;
+                agent.SetDestination(target.getTargetPosition());
+            }
+            else
+            {
+                target = null;
+                agent.isStopped = true;
+            }
         }
-
     }
-	override
-	public void attack(){
 
-	}
+    override
+    public void attack()
+    {
+        if (HitTarget != null && target.attackTarget != null)
+        {
+            attackTimer += Time.deltaTime;
+            if (attackTimer >= AttackSpeed)
+            {
+
+                target.attackTarget.GetComponent<HitInterface>().Hit(Damage);
+                attackTimer = 0;
+            }
+        }
+        else
+            target = null;
+    }
 
     public void Hit(int damage)
     {
-
+        Health -= damage;
     }
 }
