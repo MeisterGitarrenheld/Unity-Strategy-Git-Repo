cc0c42e2ab794c9a8ddb76da35fd4055c95dcae7
MeisterGitarrenheld <tom-haeschke@hotmail.de>
2018-10-14 13:19:54 +0200

    intro Scene

---
+++ Project Folder/Strategie Alpha/Assets/Manager/StartMaster.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class StartMaster : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void StartGame(int type)
+    {
+        switch (type)
+        {
+            case 0:
+                SceneManager.LoadScene(1);
+                break;
+            default: break;
+        }
+    }
+}
